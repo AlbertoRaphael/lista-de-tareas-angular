709632901c7089b9a2c8319e03e7224377f97eaf
Alberto <106848675+AlbertoRaphael@users.noreply.github.com>
2023-09-09 18:34:09 -0600

    testing de componente independiente
    testing de componente independiente

--- src/app/components/standalone/standalone.component.spec.ts
+++ src/app/components/standalone/standalone.component.spec.ts
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
         StandaloneComponent
       ],
     }).compileComponents();
-  });
+  }); 
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(StandaloneComponent);
