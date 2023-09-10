6c3e0b50c32e64bc318786c7d86f878395374be1
Alberto <106848675+AlbertoRaphael@users.noreply.github.com>
2023-09-10 04:59:40 -0600

    Delete tarea-pendiente.spec.ts

--- src/app/tarea-pendiente.spec.ts
+++
@@ -1,8 +0,0 @@
-
-import { TareaPendiente } from './tarea-pendiente';
-
-describe('TareaPendiente', () => {
-  it('should create an instance', () => {
-    expect(new TareaPendiente()).toBeTruthy();
-  });
-}); 
